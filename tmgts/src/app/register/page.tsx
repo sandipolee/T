ddce2aef6d43f56adb71b4eb0d86df98c7950c7a
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2024-06-21 12:35:12 +0545

    new
    fsfs

--- tmgts/src/app/register/page.tsx
+++ tmgts/src/app/register/page.tsx
@@ -164,6 +164,34 @@ const Register = () => {
                 id="name"
               ></input>
             </div>
+            <div className="mt-4 w-full">
+              <label
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="name"
+              >
+                Full Name
+              </label>
+              <input
+                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="text"
+                placeholder="Enter your name"
+                id="name"
+              ></input>
+            </div>
+            <div className="mt-4 w-full">
+              <label
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="name"
+              >
+                Full Name
+              </label>
+              <input
+                className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="text"
+                placeholder="Enter your name"
+                id="name"
+              ></input>
+            </div>
             </div>
           </div>
         </div>
