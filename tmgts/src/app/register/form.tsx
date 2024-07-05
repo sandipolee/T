1adaf29f41351b3081a19bd5041901f6469587d0
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2024-07-05 18:28:54 +0545

    getting okay

--- tmgts/src/app/register/form.tsx
+++ tmgts/src/app/register/form.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,68 +13,203 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast, { Toaster } from "react-hot-toast";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { NepaliDatePicker } from "nepali-datepicker-reactjs";
+import { useState } from "react";
+
+const pick_location = [
+  {
+    value: "tulsipur",
+    display: "Tulsipur",
+  },
+  {
+    value: "salyan",
+    display: "Salyan",
+  },
+  {
+    value: "Luham",
+    display: "Luham",
+  },
+  {
+    value: "kavra",
+    display: "kavra",
+  },
+  {
+    value: "darimjula",
+    display: "Darimjula",
+  },
+  {
+    value: "phalbang",
+    display: "Phalabang",
+  },
+  {
+    value: "Kathmandu",
+    display: "Kathmandu",
+  },
+  {
+    value: "palpa",
+    display: "Palpa",
+  },
+  {
+    value: "rolpa",
+    display: "rolpa",
+  },
+  {
+    value: "lanti",
+    display: "lanti",
+  },
+];
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  phone: z.string().min(10, {
+    message: "phonemost be 10 characters.",
+  }),
+  location: z.string(),
+  dob:z.string(),
+  gender: z.enum(["male", "female", "other"], {
+    required_error: "Gender is required",
   }),
 })
+;
 
 function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log("this");
-    console.log(JSON.stringify(data));
-
-    toast({
-        title: "Uh oh! Something went wrong.",
-        description: "There was a problem with your request.",
-      })
-    
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <>
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data)}</code>
-        </pre>
-        <p>{ JSON.stringify(data)}</p>
-        </>
-      ),
-    })
-  }
+  toast( <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+  </pre>);
+  
+}
 
 export function InputForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      phone: "",
+      location: "",
+      dob:""
     },
-  })
+  });
+  const listItems = pick_location.map((data, index) => (
+    <SelectItem value={data.value}> {data.display} </SelectItem>
+  ));
+
+  const [date, setDate] = useState<string>("");
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
-}
+    <>
+      <Toaster />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-2/3 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <Input placeholder="Phone" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>pick up location</FormLabel>
+                <FormControl>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select location" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>{listItems}</SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="dob"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Date of birth</FormLabel>
+                <FormControl>
+                  <NepaliDatePicker
+                    inputClassName="form-control flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    className="mt-2"
+                    value= "" 
+                    onChange={field.onChange}
+                    options={{ calenderLocale: "ne", valueLocale: "en" }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Gender</FormLabel>
+                <FormControl>
+                <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select location" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                    
+                    <SelectItem value="male">Male</SelectItem>
+                    <SelectItem value="female">Female</SelectItem>
+                    <SelectItem value="other">Other</SelectItem>
+                    </SelectContent>
+                  </Select>
 
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+}
--- tmgts/src/app/register/page.tsx
+++ tmgts/src/app/register/page.tsx
@@ -4,11 +4,10 @@ import { useState } from "react";
 import Footer from "../component/footer";
 import { NepaliDatePicker } from "nepali-datepicker-reactjs";
 import "nepali-datepicker-reactjs/dist/index.css"
-import { toast } from "@/components/ui/use-toast";
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { InputForm } from "./form";
-import { ToastWithTitle } from "./test";
+
 
 
 const Register = () => {
@@ -18,7 +17,6 @@ const Register = () => {
       <div className="h-full mx-auto w-full max-w-screen-x1 px-2.5 md:px-20 items-center">
       <div className="grid grid-cols-2 gap-10 py-10 max-sm:grid-cols-none max-sm:px-4">
       <InputForm></InputForm>
-      <ToastWithTitle></ToastWithTitle>
     </div>
       </div>
       <Footer></Footer>
