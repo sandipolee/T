0cc90abd91328b1e8e48e27cbdadfae97485fcbe
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2024-07-06 11:48:19 +0545

    5.0

--- tmgts/src/app/login/page.tsx
+++ tmgts/src/app/login/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
 
-export function LoginPAge() {
+export function Home() {
   return (
     <section>
       <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
