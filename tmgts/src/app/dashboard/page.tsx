316171eb55c2188fa768025e4bd3d35b5ea1546b
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2024-07-06 11:12:47 +0545

    new

--- tmgts/src/app/dashboard/page.tsx
+++ tmgts/src/app/dashboard/page.tsx
@@ -7,33 +7,33 @@ import { DataTable } from "./data-table"
 
 
 
-// async function getData(): Promise<Payment[]> {
-//   // Fetch data from your API here.
-//   return [
-//     {
-//       id: "728ed52f",
-//       amount: 100,
-//       status: "verified",
-//       email: "m@example.com",
-//     },
-//     {
-//       id: "728ed52f",
-//       amount: 100,
-//       status: "verified",
-//       email: "m@example.com",
-//     },
-//     {
-//       id: "728ed52f",
-//       amount: 100,
-//       status: "verified",
-//       email: "m@example.com",
-//     }
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "verified",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "verified",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "verified",
+      email: "m@example.com",
+    }
     
-//   ]
-// }
+  ]
+}
 
 const Dashboard=async ()=> {
-  // const data = await getData()
+  const data = await getData()
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
      <Sidebar></Sidebar>
@@ -41,6 +41,7 @@ const Dashboard=async ()=> {
         <Navbar></Navbar>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
         <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
         </main>
       </div>
